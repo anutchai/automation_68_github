499067412540e0683dae82b7a0d67525a4c8b9db
anutchai <anuschai.bsod@gmail.com>
2025-09-22 16:33:12 +0700

    add new case

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,8 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('example test', async ({ page }) => {
+  await page.goto('https://examplecom');
+});
